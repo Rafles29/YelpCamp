01bae6e8c5875e2dfe70d83ff62029b84fc384b5
Rafles29 <rwozniak@intelgraf.com.pl>
2017-04-10 00:37:39 +0200

    updated look of campgrounds page

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ var camps = [
     {name: "Salmon Creek", image:"https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
     {name: "Granite Hill", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
     {name: "Mountatin Goat's Rest", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+    {name: "Salmon Creek", image:"https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+    {name: "Granite Hill", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
+    {name: "Mountatin Goat's Rest", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+    {name: "Salmon Creek", image:"https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+    {name: "Granite Hill", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
+    {name: "Mountatin Goat's Rest", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"}
 ];
 
 
