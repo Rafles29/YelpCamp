d001637cd19d615f9b3ca2e8b7c2e3a0de40da88
Rafles29 <rwozniak@intelgraf.com.pl>
2017-04-17 16:15:57 +0200

    changed database schema and updated routes + views

--- app.js
+++ app.js
@@ -13,9 +13,21 @@ app.set('view engine', 'ejs');
 mongoose.connect("mongodb://localhost/yelp_camp");
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 var Campground = mongoose.model("Campground", campgroundSchema);
+
+/*Campground.create({
+    name: "Salmon Creek", image:"https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg", description:"This is a huge Salmon Creek. No water. Marvelous view!"
+}, function (err, campground) {
+    if(err){
+        console.log(err)
+    }else {
+        console.log("NEWLY CREATED CAMPGROUND: ");
+        console.log(campground);
+    }
+});*/
 
 
 //routing
@@ -29,21 +41,17 @@ app.get("/campgrounds", function (req, res) {
            console.log(err);
        }
        else {
-           res.render("campgrounds", {campgrounds:allCampgrounds});
+           res.render("index", {campgrounds:allCampgrounds});
        }
     });
 });
 
-app.get("/campgrounds/new", function (req, res) {
-    res.render("new");
-});
-
 app.post("/campgrounds", function (req, res) {
     Campground.create({
-        name: req.body.name, image:req.body.image
+        name: req.body.name, image:req.body.image, description:req.body.description
     }, function (err, campground) {
         if(err){
-            console.log(err)
+            console.log(err);
         }else {
             console.log("NEWLY CREATED CAMPGROUND: ");
             console.log(campground);
@@ -51,6 +59,21 @@ app.post("/campgrounds", function (req, res) {
     });
 
     res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function (req, res) {
+    res.render("new");
+});
+
+app.get("/campgrounds/:id", function (req, res) {
+    Campground.findById(req.params.id, function (err, foundCampground) {
+        if(err) {
+            console.log(err);
+        }else {
+            res.render('show', {campground: foundCampground});
+        }
+    });
+
 });
 
 //port listening
