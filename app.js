ddb6211c280154f64460e002b990318cd2590c93
Rafles29 <rwozniak@intelgraf.com.pl>
2017-04-17 15:37:16 +0200

    added connection to the database

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
+//dependencies
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//initial setting
 var app = express();
-
-//camps
-var camps = [
-    {name: "Salmon Creek", image:"https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
-    {name: "Granite Hill", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
-    {name: "Mountatin Goat's Rest", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-    {name: "Salmon Creek", image:"https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
-    {name: "Granite Hill", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
-    {name: "Mountatin Goat's Rest", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-    {name: "Salmon Creek", image:"https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
-    {name: "Granite Hill", image:"https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg"},
-    {name: "Mountatin Goat's Rest", image:"https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"}
-];
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.set('view engine', 'ejs');
 
+//database
+mongoose.connect("mongodb://localhost/yelp_camp");
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+
+//routing
 app.get("/", function (req, res) {
     res.render("landing");
 });
 app.get("/campgrounds", function (req, res) {
-    res.render("campgrounds", {campgrounds:camps});
+
+    Campground.find({}, function (err, allCampgrounds) {
+       if(err) {
+           console.log(err);
+       }
+       else {
+           res.render("campgrounds", {campgrounds:allCampgrounds});
+       }
+    });
 });
 
 app.get("/campgrounds/new", function (req, res) {
@@ -33,13 +39,21 @@ app.get("/campgrounds/new", function (req, res) {
 });
 
 app.post("/campgrounds", function (req, res) {
-    console.log(req.body);
-    camps.push({name: req.body.name,image:req.body.image});
+    Campground.create({
+        name: req.body.name, image:req.body.image
+    }, function (err, campground) {
+        if(err){
+            console.log(err)
+        }else {
+            console.log("NEWLY CREATED CAMPGROUND: ");
+            console.log(campground);
+        }
+    });
+
     res.redirect("/campgrounds");
 });
 
+//port listening
 app.listen('3000', function () {
    console.log("The YelpCamp server has started")
 });
-
-
